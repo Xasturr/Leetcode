eeb7f95120b5116e4249b79cc8611cad867571f4
Leshchenko Dmytro <dima.leshchenko20@gmail.com>
2026-04-17 18:04:50 +0300

    Beats 42.89%

---
+++ 3761. Minimum Absolute Distance Between Mirror Pairs/main.cpp
@@ -0,0 +1,29 @@
+﻿class Solution {
+public:
+    int minMirrorPairDistance(vector<int>& nums)
+    {
+        std::unordered_map<int, int> map;
+        int distance = -1;
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            std::string numString = std::to_string(nums[i]);
+            std::reverse(numString.begin(), numString.end());
+            const int numReversed = std::stoi(numString);
+
+            if (map.contains(nums[i]))
+            {
+                distance = distance == - 1 ? i - map[nums[i]] : std::min(i - map[nums[i]], distance);
+
+                if (distance == 1)
+                {
+                    return distance;
+                }
+            }
+
+            map[numReversed] = i;
+        }
+
+        return distance;
+    }
+};
