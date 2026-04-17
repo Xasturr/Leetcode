861955ab4a34a97578599ae92f30787741d120a4
Leshchenko Dmytro <dima.leshchenko20@gmail.com>
2026-04-17 21:16:48 +0300

    Beats 100.00%

---
+++ 1544. Make The String Great/main.cpp
@@ -0,0 +1,20 @@
+﻿string makeGood(string s)
+{
+    bool erased = false;
+    do
+    {
+        erased = false;
+        for (int i = 0; i < s.length() - 1 && !s.empty(); ++i)
+        {
+            if (s[i] != s[i + 1] && std::tolower(s[i]) == std::tolower(s[i + 1]))
+            {
+                s.erase(i, 2);
+                erased = true;
+
+                break;
+            }
+        }
+    } while (erased);
+
+    return s;
+}
