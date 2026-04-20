0da82c36d3fd1e5e78d59181bde35febae2f45ba
Leshchenko Dmytro <dima.leshchenko20@gmail.com>
2026-04-20 17:13:24 +0300

    Beats 100.00%

---
+++ 2078. Two Furthest Houses With Different Colors/main.cpp
@@ -0,0 +1,21 @@
+﻿class Solution {
+public:
+    int maxDistance(vector<int>& colors) 
+    {
+        int maxDistance = 1;
+
+        for (int i = 0; i < colors.size(); ++i)
+        {
+
+            for (int j = colors.size() - 1; j >= 0; --j)
+            {
+                if (colors[i] != colors[j])
+                {
+                    maxDistance = max(maxDistance, abs(j - i));
+                }
+            }
+        }
+
+        return maxDistance;
+    }
+};
