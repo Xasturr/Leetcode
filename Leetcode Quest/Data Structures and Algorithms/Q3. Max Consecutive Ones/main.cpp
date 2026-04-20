23056133f27fa882b9195ad68bcc2e0087b65c08
Leshchenko Dmytro <dima.leshchenko20@gmail.com>
2026-04-20 20:16:14 +0300

    Beats 100.00%

---
+++ Leetcode Quest/Data Structures and Algorithms/Q3. Max Consecutive Ones/main.cpp
@@ -0,0 +1,25 @@
+﻿class Solution 
+{
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums)
+    {
+        int maxConsecutiveOnes = 0;
+        int currentConsecutiveOnes = 0;
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (nums[i] == 0)
+            {
+                maxConsecutiveOnes = max(maxConsecutiveOnes, currentConsecutiveOnes);
+                currentConsecutiveOnes = 0;
+            }
+            else
+            {
+                ++currentConsecutiveOnes;
+            }
+        }
+
+        maxConsecutiveOnes = max(maxConsecutiveOnes, currentConsecutiveOnes);
+        return maxConsecutiveOnes;
+    }
+};
