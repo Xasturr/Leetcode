9b7fecf09e0477ebd5cd35bbb06d8381aca44e04
Leshchenko Dmytro <dima.leshchenko20@gmail.com>
2026-04-20 19:54:39 +0300

    Beats 100.00%

---
+++ Leetcode Quest/Data Structures and Algorithms/Q2. Shuffle the Array/main.cpp
@@ -0,0 +1,15 @@
+﻿class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector <int> result(2 * n);
+        int* xPtr = &nums[0];
+        int* yPtr = &nums[n];
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            result[i] = i % 2 ? *yPtr++ : *xPtr++;
+        }
+
+        return result;
+    }
+};
