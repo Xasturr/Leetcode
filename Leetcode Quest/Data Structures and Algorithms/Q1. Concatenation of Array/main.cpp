acb4c1a8480a421e5878b72dc29d5c4e9ea2f8a4
Leshchenko Dmytro <dima.leshchenko20@gmail.com>
2026-04-20 17:55:26 +0300

    Beats 100.00%

---
+++ Leetcode Quest/Data Structures and Algorithms/Q1. Concatenation of Array/main.cpp
@@ -0,0 +1,14 @@
+﻿class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) 
+    {
+        const int numsSize = nums.size();
+
+        for (int i = 0; i < numsSize; i++)
+        {
+            nums.push_back(nums[i]);
+        }
+
+        return nums;
+    }
+};
