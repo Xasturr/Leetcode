97286871df564d1409af8a05f83501a1dec09b70
Leshchenko Dmytro <dima.leshchenko20@gmail.com>
2026-04-21 19:50:33 +0300

    Beats 100.00%

---
+++ Leetcode Quest/Data Structures and Algorithms/Stack/Q1. Build an Array With Stack Operations/main.cpp
@@ -0,0 +1,30 @@
+﻿class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) 
+    {
+        vector<string> result;
+        auto it = target.begin();
+        int currTarget = *it;
+
+        for (int i = 1; i <= n; ++i)
+        {
+            result.push_back("Push");
+
+            if (i == currTarget)
+            {
+                if (it + 1 == target.end())
+                {
+                    return result;
+                }
+
+                currTarget = *(++it);
+            }
+            else if (i != currTarget)
+            {
+                result.push_back("Pop");
+            }
+        }
+
+        return result;
+    }
+};
