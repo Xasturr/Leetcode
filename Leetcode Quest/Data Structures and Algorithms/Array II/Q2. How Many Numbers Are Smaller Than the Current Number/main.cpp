4c9961352c2756d82ff3da496cabe2fc29dfe36a
Leshchenko Dmytro <dima.leshchenko20@gmail.com>
2026-04-21 18:06:50 +0300

    Beats 68.57%

---
+++ Leetcode Quest/Data Structures and Algorithms/Array II/Q2. How Many Numbers Are Smaller Than the Current Number/main.cpp
@@ -0,0 +1,29 @@
+﻿class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) 
+    {
+        vector<int> res(nums.size());
+        map<int, int> map;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            map[nums[i]]++;
+        }
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (auto iter : map)
+            {
+                if (iter.first >= nums[i])
+                {
+                    break;
+                }
+
+                res[i] += iter.second;
+            }
+        }
+
+
+        return res;
+    }
+};
