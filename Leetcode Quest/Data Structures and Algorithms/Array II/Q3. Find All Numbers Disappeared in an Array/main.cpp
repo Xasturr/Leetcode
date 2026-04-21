34014d7449cc7928f02f07d8cbc64e5baf595498
Leshchenko Dmytro <dima.leshchenko20@gmail.com>
2026-04-21 19:12:53 +0300

    Beats 100.00%

---
+++ Leetcode Quest/Data Structures and Algorithms/Array II/Q3. Find All Numbers Disappeared in an Array/main.cpp
@@ -0,0 +1,28 @@
+﻿class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) 
+    {
+        vector<int> result;
+        vector<int> numsCopy(nums.size());
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (numsCopy[nums[i] - 1] == nums[i])
+            {
+                continue;
+            }
+
+            numsCopy[nums[i] - 1] = nums[i];
+        }
+
+        for (int i = 0; i < numsCopy.size(); ++i)
+        {
+            if (numsCopy[i] == 0)
+            {
+                result.push_back(i + 1);
+            }
+        }
+
+        return result;
+    }
+};
