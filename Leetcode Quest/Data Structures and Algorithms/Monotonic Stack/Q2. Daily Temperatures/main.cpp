7008a8b8cdfdbaf42d7e6539fa9e9106f6332e21
Leshchenko Dmytro <dima.leshchenko20@gmail.com>
2026-04-22 12:45:19 +0300

    Beats 71.62%

---
+++ Leetcode Quest/Data Structures and Algorithms/Monotonic Stack/Q2. Daily Temperatures/main.cpp
@@ -0,0 +1,22 @@
+﻿class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures)
+    {
+        vector<int> result(temperatures.size(), 0);
+        stack<int> s;
+
+        for (int i = 0; i < temperatures.size(); ++i)
+        {
+            while (!s.empty() && temperatures[s.top()] < temperatures[i])
+            {
+                int index = s.top();
+                s.pop();
+                result[index] = i - index;
+            }
+
+            s.push(i);
+        }
+
+        return result;
+    }
+};
