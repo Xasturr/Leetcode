68fd75c8c848150f7089d272d91c350806cb0d83
Leshchenko Dmytro <dima.leshchenko20@gmail.com>
2026-04-22 12:32:09 +0300

    Beats 100.00%

--- Leetcode Quest/Data Structures and Algorithms/Monotonic Stack/Q1. Final Prices With a Special Discount in a Shop/main.cpp
+++ Leetcode Quest/Data Structures and Algorithms/Monotonic Stack/Q1. Final Prices With a Special Discount in a Shop/main.cpp
@@ -3,17 +3,18 @@ public:
     vector<int> finalPrices(vector<int>& prices) 
     {
         vector<int> result = prices;
+        stack<int> s;
 
-        for (int i = 0; i < prices.size() - 1; ++i)
+        for (int i = 0; i < prices.size(); ++i)
         {
-            for (int j = i + 1; j < prices.size(); ++j)
+            while (!s.empty() && prices[s.top()] >= prices[i])
             {
-                if (prices[i] >= prices[j])
-                {
-                    result[i] = prices[i] - prices[j];
-                    break;
-                }
+                const int index = s.top();
+                s.pop();
+                result[index] = prices[index] - prices[i];
             }
+
+            s.push(i);
         }
 
         return result;
