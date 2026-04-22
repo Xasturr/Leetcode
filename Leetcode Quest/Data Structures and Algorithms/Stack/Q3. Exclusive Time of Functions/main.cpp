ab37511d3ceee9d29315004f4648062bce40eab5
Leshchenko Dmytro <dima.leshchenko20@gmail.com>
2026-04-22 09:19:02 +0300

    Beats 80.94%

---
+++ Leetcode Quest/Data Structures and Algorithms/Stack/Q3. Exclusive Time of Functions/main.cpp
@@ -0,0 +1,58 @@
+﻿class Solution {
+public:
+    struct Log
+    {
+        int functionId;
+        string state;
+        int timestamp;
+    };
+
+    vector<int> exclusiveTime(int n, vector<string>& logs)
+    {
+        vector<int> result(n, 0);
+        stack<Log> s;
+        Log lastLog;
+
+        for (const string& logString : logs)
+        {
+            stringstream ss(logString);
+            string functionId, state, timestamp;
+
+            getline(ss, functionId, ':');
+            getline(ss, state, ':');
+            getline(ss, timestamp, ':');
+
+            Log currLog = {stoi(functionId), state, stoi(timestamp)};
+
+            if (currLog.state == "start")
+            {
+                if (!s.empty())
+                {
+                    result[s.top().functionId] += currLog.timestamp - lastLog.timestamp;
+
+                    if (lastLog.state == "end")
+                    {
+                        result[s.top().functionId] -= 1;
+                    }
+                }
+
+                s.push(currLog);
+            }
+            else
+            {
+                s.pop();
+
+                result[currLog.functionId] += currLog.timestamp - lastLog.timestamp;
+
+                if (lastLog.state == "start")
+                {
+                    result[currLog.functionId] += 1;
+                }
+            }
+
+            lastLog = currLog;
+        }
+
+        return result;
+    }
+};
