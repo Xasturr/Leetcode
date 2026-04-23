ad44d17ad3dc695776b371e86050ebad4c24b4e2
Leshchenko Dmytro <dima.leshchenko20@gmail.com>
2026-04-23 16:29:31 +0300

    Beats 69.30%

---
+++ 1685. Sum of Absolute Differences in a Sorted Array/main.cpp
@@ -0,0 +1,23 @@
+﻿class Solution {
+public:
+    vector<int> getSumAbsoluteDifferences(vector<int>& nums)
+    {
+        vector<int> result(nums.size());
+        int leftSum = 0;
+        int sum = 0;
+
+        for (const int num : nums)
+        {
+            sum += num;
+        }
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            const int rightSum = sum - nums[i] - leftSum; 
+            result[i] = i * nums[i] - leftSum + rightSum - (nums.size() - i - 1) * nums[i];
+            leftSum += nums[i];
+        }
+
+        return result;   
+    }
+};
