19edd9d2848d016ee6128dc2c5fcad36f4640488
Leshchenko Dmytro <dima.leshchenko20@gmail.com>
2026-04-24 13:55:39 +0300

    Beats 100.00%

---
+++ 2833. Furthest Point From Origin/main.cpp
@@ -0,0 +1,27 @@
+﻿class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) 
+    {
+        int leftMoves = 0;
+        int rightMoves = 0;
+        int anyMoves = 0;
+
+        for (const char move : moves)
+        {
+            if (move == 'L')
+            {
+                ++leftMoves;
+            }
+            else if (move == 'R')
+            {
+                ++rightMoves;
+            }
+            else
+            {
+                ++anyMoves;
+            }
+        }
+
+        return max(leftMoves - rightMoves, rightMoves - leftMoves) + anyMoves;
+    }
+};
